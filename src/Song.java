c03a113f5b768e9d2eb759e42ae4005250fcd951
David Yin <david.yin@austinisd.org>
2021-08-17 17:20:25 -0500

    Initial commit

---
+++ src/Song.java
@@ -0,0 +1,16 @@
+public class Song {
+    private String title;
+
+    public Song(){
+        this.title = "Hell's Bells";
+    }
+
+    public void setTitle(String t){
+        this.title = t;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+}
